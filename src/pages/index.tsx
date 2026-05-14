ca18061fc6343c4f7fb9a50de99374cade27c4eb
Wilson <wilson.diant@gmail.com>
2026-05-14 18:22:31 -0400

    Enhance UI components and improve styling for better user experience

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,32 +17,42 @@ export default function Home() {
 		tag: group.tag,
 		length: group.videos.length,
 	}));
-	const filteredTag = grouped.filter((group) => group.tag === getTags);
+	const filteredTag = grouped.filter(
+		(group) => getTags === '' || group.tag === getTags,
+	);
 	const allVideos = filteredTag.flatMap((group) => group.videos);
 	console.log(allVideos);
 	return (
 		<div className='h-screen w-screen p-4'>
 			<div className=' grid grid-cols-5 grid-rows-7 gap-4 h-full '>
-				<nav className=' rounded col-span-full bg-blue-400'>.</nav>
+				<nav className=' rounded col-span-full bg-blue-400'>
+					<h1>Watched Videos</h1>
+					<div>{allVideos.length}</div>
+					<button>search</button>
+				</nav>
 				<aside className='relative rounded row-span-6 bg-red-500 h-0min-h-0 overflow-hidden'>
-					<div className='absolute top-0 left-0 bg-gray-400 w-full h-10 pb-10'>
-						.
-					</div>
+					<h2
+						onClick={() => {
+							setTags('');
+							setSubcategory([]);
+						}}
+						className='cursor-pointer absolute top-0 left-0 bg-gray-400 w-full h-10 pb-10'
+					>
+						all videos
+					</h2>
 					<ul className='overflow-y-scroll w-auto h-full min-h-0 py-10 scrollbar-none'>
-						<li>tags</li>
+						<h2>tags</h2>
 						{tagList.map((tag) => (
 							<li className=' ' key={tag.tag}>
 								<button
-									className='cursor-pointer'
+									className='cursor-pointer flex justify-between w-full px-2 pb-1  hover:bg-gray-400'
 									onClick={() => {
 										setTags(tag.tag);
 										setSubcategory([]);
 									}}
 								>
 									<span>{tag.tag}</span>
-									<span className='ml-2 text-sm text-gray-500'>
-										{tag.length}
-									</span>
+									<span className='ml-2  text-gray-500'>{tag.length}</span>
 								</button>
 							</li>
 						))}
@@ -50,27 +60,52 @@ export default function Home() {
 				</aside>
 				<main className='rounded row-span-6 col-span-4 h-full min-h-0 overflow-hidden'>
 					<section className='h-full min-h-0 flex flex-col gap-4'>
-						<div className='rounded bg-gray-400 w-auto h-auto'>
-							{[
-								...new Set(allVideos.map((video) => video.subcategory).flat()),
-							].map((subcategory) => (
-								<button
-									onClick={() =>
-										setSubcategory((prev) => {
-											if (prev.includes(subcategory)) {
-												return prev.filter((item) => item !== subcategory);
-											} else {
-												return [...prev, subcategory];
-											}
-										})
-									}
-									key={subcategory}
-									className='mr-2 cursor-pointer inline'
-								>
-									{subcategory}
-								</button>
-							))}
-						</div>
+						{getTags && (
+							<div className='rounded bg-gray-400 w-auto h-auto'>
+								<div>
+									<h2>concept</h2>
+									{[
+										...new Set(allVideos.map((video) => video.subcategory[0])),
+									].map((concept, index) => (
+										<span className=' bg-red-400 mx-1' key={index}>
+											{concept}
+										</span>
+									))}
+								</div>
+								<div>
+									<h2>tools</h2>
+									{[
+										...new Set(allVideos.map((video) => video.subcategory[1])),
+									].map((concept, index) => (
+										<span className=' bg-red-400 mx-1' key={index}>
+											{concept}
+										</span>
+									))}
+								</div>
+								<div>
+									<h2>topics</h2>
+									{[
+										...new Set(allVideos.map((video) => video.subcategory[2])),
+									].map((concept, index) => (
+										<span className=' bg-red-400 mx-1' key={index}>
+											{concept}
+										</span>
+									))}
+								</div>
+								{/* <div>
+									<h1>tools</h1>
+									{allVideos.map((video, index) => (
+										<span key={index}>{video.subcategory[1]}</span>
+									))}
+								</div>
+								<div>
+									<h1>topics</h1>
+									{allVideos.map((video, index) => (
+										<span key={index}>{video.subcategory[2]}</span>
+									))}
+								</div> */}
+							</div>
+						)}
 						<div className='rounded bg-gray-300 w-auto h-8'></div>
 						<section
 							className={`${allVideos.length < 9 ? 'grid-rows-3' : 'xl:grid-rows-3'} grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 flex-1 min-h-0 overflow-y-auto scrollbar-none`}
@@ -86,19 +121,30 @@ export default function Home() {
 								.map((video) => (
 									<div
 										key={video.index}
-										className='rounded bg-gray-200 w-auto h-50'
+										className='rounded bg-gray-200 w-auto h-56'
 									>
-										<div className='rounded bg-gray-400 w-auto h-10'>
-											{video.title}
-										</div>
 										<Image
 											src={`https://i.ytimg.com/vi/${getYouTubeId(video.link)}/mqdefault.jpg`}
 											alt={video.title}
 											width={300}
 											height={200}
 										/>
-										<div className='rounded bg-gray-300 w-auto h-8'>
+										<h1 className='rounded text-sm bg-gray-400 w-auto h-10 overflow-hidden'>
+											{video.title}
+										</h1>
+										<h2 className='rounded text-xs  bg-green-300 w-auto h-auto'>
 											{video.channelName}
+										</h2>
+										<div className='rounded  text-xs w-auto flex flex-wrap gap-1 '>
+											<span className='bg-red-200 w-auto h-auto '>
+												{video.subcategory[0]}
+											</span>
+											<span className='bg-red-200 w-auto h-auto'>
+												{video.subcategory[1]}
+											</span>
+											<span className='bg-red-200 w-auto h-auto'>
+												{video.subcategory[2]}
+											</span>
 										</div>
 									</div>
 								))}

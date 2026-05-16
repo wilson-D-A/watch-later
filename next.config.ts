1c5c3e1a4423f61c6068cab8b78b237d432b45e0
Wilson <wilson.diant@gmail.com>
2026-05-16 15:29:12 -0400

    Update next.config.ts to remove basePath and add index.tsx for WatchLater component

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 
-	basePath: '/project',
+
 	output: 'standalone',
 	reactCompiler: true,
 	reactStrictMode: true,
